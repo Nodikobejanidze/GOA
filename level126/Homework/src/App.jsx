6fa100c83e8df8c07e39685feeb9ec970f02b651
NodikoBejanidze <bejanidzenodiko@gmail.com>
2024-11-22 17:11:12 +0400

    second commit

---
+++ level126/Homework/src/App.jsx
@@ -0,0 +1,12 @@
+const items = ["Apple", "Banana", "Cherry"];
+function App() {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
