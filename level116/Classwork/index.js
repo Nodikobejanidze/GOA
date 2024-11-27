f3aa7d72044aa0269dc79c60a673fac23a04a13c
NodikoBejanidze <bejanidzenodiko@gmail.com>
2024-11-27 16:33:47 +0400

    commit

--- level116/Classwork/index.js
+++ level116/Classwork/index.js
@@ -3,9 +3,9 @@ const { greet, calculateAverage } = require('./Functions');
 
 
 const greeting = greet('Nika');
-console.log(greeting); // "Hello, Nika!"
+console.log(greeting); 
 
 
 const numbers = [5, 10, 15, 20];
 const average = calculateAverage(numbers);
-console.log(`The average is: ${average}`); // "The average is: 12.5"
+console.log(`The average is: ${average}`); 
